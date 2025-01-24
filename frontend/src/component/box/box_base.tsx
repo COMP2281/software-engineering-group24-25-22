6c5dbb71e663b5287291ff45e3b83bc65bb6521f
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2025-01-24 11:48:53 +0000

    fix the box base component

--- frontend/src/component/box/box_base.tsx
+++ frontend/src/component/box/box_base.tsx
@@ -3,21 +3,33 @@ import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 
 export function BoxBase() {
-    const itemName = ['receipt1', 'receipt2'];
+    const itemName = [
+        {
+            title: 'Receipt1',
+            content: 'stuff stuff stuff stuff stuff stuff stuff',
+        },
+        {
+            title: 'Receipt2',
+            content: 'new stuff new stuff new stuff stuff',
+        },
+    ];
 
     return (
         <div className="bg-gray-400 p-4 w-1/2 h-auto">
-            {itemName.map((value, index) => (
+            {itemName.map((item, index) => (
                 <div
                     key={index}
-                    className="flex items-center justify-between p-4 py-8 bg-gray-600 bg-opacity-30 text-3xl border-4 mb-4 last:mb-0"
+                    className="p-4 py-8 bg-gray-600 bg-opacity-30 text-3xl border-2 mb-4 last:mb-0"
                 >
-                    <div className="text-black text-xl">
-                        {value}
+                    <div className="flex items-center justify-between">
+                        <div className="text-black text-xl font-bold">{item.title}</div>
+                        <IconButton aria-label="edit">
+                            <EditIcon />
+                        </IconButton>
                     </div>
-                    <IconButton aria-label="comment">
-                        <EditIcon />
-                    </IconButton>
+                    <div className="text-black text-lg pl-4">
+                        {item.content}
+                    </div>
                 </div>
             ))}
         </div>
