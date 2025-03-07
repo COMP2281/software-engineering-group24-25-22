41300a3dce610fb87b241b469e13e5f88bcc2f32
Your GitHub SatapasT <satapastanachotpaisit@gmail.com>
2025-03-07 12:31:28 +0000

    Testing out the end point

---
+++ frontend/src/component/buttons/login_button.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+export function LoginButton() {
+    return (
+        <div> 
+            <Button 
+                variant="contained" 
+            >
+                Login
+            </Button>
+        </div>
+    );
+}
